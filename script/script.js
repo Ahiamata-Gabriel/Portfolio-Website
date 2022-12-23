807a8c7437eab768b46f8bc44820d18f4ae722f5
Ahiamata-Gabriel <ahiamatamawuli@gmail.com>
2022-12-23 00:47:34 +0000

    Worked on skills and project section for mobile views

--- script/script.js
+++ script/script.js
@@ -1,3 +1,12 @@
+const scrollHeader = () => {
+  const header = document.getElementById('header');
+  const topElement = document.querySelector('.header');
+  this.scrollY >= 50
+    ? topElement.classList.add('scroll-header')
+    : topElement.classList.remove('scroll-header');
+};
+window.addEventListener('scroll', scrollHeader);
+
 const moonToggle = document.getElementById('moon-icon');
 const body = document.querySelector('body');
 
